0fa647f712e4fdeb9c2db4eb9ba0a1aa6d616ee9
Allan Valle <allanvalle@outlook.com>
2023-03-08 14:13:18 -0300

    Rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'chatrooms/show'
   devise_for :users
+  resources :chatrooms, only: :show
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
